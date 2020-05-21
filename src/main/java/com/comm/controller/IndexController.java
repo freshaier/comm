58b11f39a04310552de658ec194c88bc16215474
FreshAier <2995747324@qq.com>
2020-05-21 17:10:40 +0800

    add index and bootstrap

---
+++ src/main/java/com/comm/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.comm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    /*public String Hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+
+    }*/
+    public String index(){
+        return "index";
+    }
+}
